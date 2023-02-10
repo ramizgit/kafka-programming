0306a03d6676b10917c464f3c34554cc27cb2052
ramizgit <65121963+ramizgit@users.noreply.github.com>
2023-02-10 07:08:25 +0530

    Update MyKafkaProducerWithProtobufModel.java

--- src/main/java/com/kafka/producer/MyKafkaProducerWithProtobufModel.java
+++ src/main/java/com/kafka/producer/MyKafkaProducerWithProtobufModel.java
@@ -16,10 +16,7 @@ public class MyKafkaProducerWithProtobufModel {
         System.out.println("going to publish messages");
 
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092"); //c:/windows/System32/drivers/etc/hosts
-        props.put("linger.ms", 1);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("bootstrap.servers", "localhost:9092");
 
         Producer<Integer, ProtMessage> producer = new KafkaProducer<>(props, new IntegerSerializer(), new ProtMessageSerializer());
         for (int i = 31; i <= 40; i++){
