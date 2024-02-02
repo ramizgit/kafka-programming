a3c45a257fe63d8977ae4f9da15d8d796c6b3181
ramizgit <65121963+ramizgit@users.noreply.github.com>
2024-02-02 11:49:54 +0530

    Update KafkaStreamApplication.java

--- src/main/java/com/kafka/streams/KafkaStreamApplication.java
+++ src/main/java/com/kafka/streams/KafkaStreamApplication.java
@@ -1,1 +1,50 @@
+package com.kafka.streams;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class KafkaStreamApplication {
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        //builder.stream("source_kafka_stream").filter((key,value) -> (int)key % 2 == 0).to("sink_kafka_topic");
+        builder.stream("source_kafka_stream").
+                //filter((key,value) -> (int)value % 2 == 0).
+                mapValues(value -> "stream-"+value).
+                to("sink_kafka_topic");
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+        // attach shutdown handler to catch control-c
+        final CountDownLatch latch = new CountDownLatch(1);
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                System.out.println("stream closed");
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            System.out.println("stream started");
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+
+    }
+}
 
