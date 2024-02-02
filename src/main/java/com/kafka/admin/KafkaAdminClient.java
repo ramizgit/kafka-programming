7bb93702978b0f298832e83be70671752c2c44ae
ramizgit <65121963+ramizgit@users.noreply.github.com>
2024-02-02 11:49:17 +0530

    Update KafkaAdminClient.java

--- src/main/java/com/kafka/admin/KafkaAdminClient.java
+++ src/main/java/com/kafka/admin/KafkaAdminClient.java
@@ -1,1 +1,25 @@
+package com.kafka.admin;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class KafkaAdminClient {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        try(AdminClient adminClient = AdminClient.create(props)){
+            ListTopicsResult topicsResult = adminClient.listTopics();
+            Set<String> topics = topicsResult.names().get();
+            System.out.println(topics);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
